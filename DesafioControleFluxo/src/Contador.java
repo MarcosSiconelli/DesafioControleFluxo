a4064cc3a22afedee65fdb3ac2b7ece2b78ba0c6
MarcosSiconelli <marcossiconelli@icloud.com>
2024-05-22 21:49:10 -0300

    atualizado

--- DesafioControleFluxo/src/Contador.java
+++ DesafioControleFluxo/src/Contador.java
@@ -38,38 +38,9 @@ public class Contador {
 	}
 
         
-        
-        
-        
-        
-        
-        
-        /*int parametro1, parametro2;
-
-        System.out.println("Digite o primeiro parametro");
-        parametro1=terminal.nextInt();
-
-        System.out.println("Digite o segundo parametro");
-        parametro2=terminal.nextInt();
-
-        try {
-            contar(parametro1,parametro2);
-
-
-        } catch (Exception e) 
-            System.out.println("O primeiro número deve ser menor que o segundo.");
-        
-    static void contar(int parametro1; int parametro2) throws ParametrosInvalidosException {
-		//validar se parametroUm é MAIOR que parametroDois e lançar a exceção
-        if (parametro1 >parametro2) {
-            System.out.println("O primeiro número deve ser menor que o segundo.");
-        }
-		int contagem = parametro2 - parametro1;
-        for(int x=0;x>contagem; x++){
-        System.out.println("Imprimindo o número " + x);
-        }
-		
-		//realizar o for para imprimir os números com base na variável contagem
-	}
     
-*/
+        
+        
+        
+        
+    
