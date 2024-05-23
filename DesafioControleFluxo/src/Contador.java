0c363aaabcf363e8ee17b7cbc7d14bd5016da18a
MarcosSiconelli <marcossiconelli@icloud.com>
2024-05-23 08:29:52 -0300

    ultima att

--- DesafioControleFluxo/src/Contador.java
+++ DesafioControleFluxo/src/Contador.java
@@ -6,9 +6,13 @@ public class Contador {
 
     public static void main(String[] args) {		
 		try {
-			
+            Scanner terminal = new Scanner(System.in).useLocale(Locale.US);
+            System.out.println("Digite o primeiro parâmetro");
+            int parametroUm = terminal.nextInt();
+            System.out.println("Digite o segundo parâmetro");
+            int parametroDois = terminal.nextInt();
             //chamando o método contendo a lógica de contagem
-			contar();
+			contar(parametroUm,parametroDois);
 		
 		}
         catch (Exception e) {
@@ -17,12 +21,8 @@ public class Contador {
 		}
 		
 	}
-	static void contar() throws ParametrosInvalidosException {
-        Scanner terminal = new Scanner(System.in).useLocale(Locale.US);
-        System.out.println("Digite o primeiro parâmetro");
-		int parametroUm = terminal.nextInt();
-		System.out.println("Digite o segundo parâmetro");
-		int parametroDois = terminal.nextInt();
+	static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+    
 		int contagem = parametroDois - parametroUm;
 
         if (parametroUm > parametroDois) {
